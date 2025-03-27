4669e3682cc4dd8e861949bde54c12262e053c3d
harinimuruges04@gmail.com <harinimuruges04@gmail.com>
2025-03-27 15:32:26 +0530

    Apply operation to array can be added

---
+++ Array.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.print("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+
+        System.out.print("Resulting array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
