9f6b2c05f5b0bf2eb495c0687455362168d16298
harinimuruges04@gmail.com <harinimuruges04@gmail.com>
2025-04-11 14:39:51 +0530

    missing code will changed

--- missingnumber.java
+++ missingnumber.java
@@ -10,11 +10,15 @@ public class missingnumber
 		    arr[i]=sc.nextInt();
 		}
 		Arrays.sort(arr);
+        if(arr[0]!=0){
+            System.out.println(0);
+        }
 		for(int i=1;i<n;i++){
 		    int miss=arr[i]-arr[i-1];
 		    for(int j=1;j<miss;j++){
 		        System.out.print(arr[i-1]+1+" ");
 		    }
 		}
+        System.out.println(n);
 	}
 }
