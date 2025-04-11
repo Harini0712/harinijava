915a95a2e03c622a8dfa1967330c183d8e49ff3e
harinimuruges04@gmail.com <harinimuruges04@gmail.com>
2025-04-11 14:19:09 +0530

    missing number

---
+++ missingnumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class missingnumber
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for(int i=1;i<n;i++){
+		    int miss=arr[i]-arr[i-1];
+		    for(int j=1;j<miss;j++){
+		        System.out.print(arr[i-1]+1+" ");
+		    }
+		}
+	}
+}
